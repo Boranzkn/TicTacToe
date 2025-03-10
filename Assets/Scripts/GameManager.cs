cf99d63b4b233be9ee8ecfb4f79ed05deb51ad2c
Boran Ozkan <boranozkan11@gmail.com>
2025-03-11 00:10:44 +0300

    created GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void ClickedOnGridPosition(int x, int y)
+    {
+
+    }
+}
